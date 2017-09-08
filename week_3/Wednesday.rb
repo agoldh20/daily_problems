850b831d8125597d646c9b660ddbcfe8636c1dca
agoldwater <dmgldwtr@gmail.com>
2017-09-07 21:36:22 -0500

    updated code for Wednesday

--- week_3/Wednesday.rb
+++ week_3/Wednesday.rb
@@ -1,4 +1,16 @@
-def mutation?(string_1, string_2)
+def mutation?(check_word, included_in_word)
   
+  # first_word = check_word.split("")
+  second_word_array = included_in_word.split("")
+  # #first_word.include?(second_word_array)
+
+  second_word_array.each do |letter| 
+    p check_word.include?(letter)
+  end
+
   
 end
+
+mutation?("burly", "ruby")
+puts "-------------"
+mutation?("burly", "python")
